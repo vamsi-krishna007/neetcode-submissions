88070e66ca119d9e0ed114c2d855dd5fe2ef13fd
vamsi-krishna007 <67186572+vamsi-krishna007@users.noreply.github.com>
2026-06-11 20:04:08 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false;
+
+        let map = new Map();
+
+        for (let char of s) {
+            map.set(char, (map.get(char) || 0) + 1);
+        }
+
+        for (let char of t) {
+            if (!map.has(char)) {
+                return false;
+            }
+
+            map.set(char, map.get(char) - 1);
+            
+            if (map.get(char) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
